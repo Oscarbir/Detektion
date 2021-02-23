ad1ce0750d938cedf0672327776a69a49784f2fb
Oscar Birging <oscarbi@student.chalmers.se>
2021-02-23 14:15:22 +0100

    up2date

--- Detektion.m
+++ Detektion.m
@@ -1,5 +1,18 @@
 clc, clear all
 
 %%Tänker att vi skapar en signal först 
+X=load('x.mat');
+x=X.x;
+r1=xcorr(x(:,1),x(:,2),3000)/length(x(:,1));
+r2=xcorr(x(:,1),x(:,3),3000)/length(x(:,1));
+r3=xcorr(x(:,1),x(:,4),3000)/length(x(:,1));
+r4=xcorr(x(:,1),x(:,5),3000)/length(x(:,1));
+ subplot(1,4,1)
+plot(abs(r1))
+ subplot(1,4,2)
+ plot(abs(r2))
+subplot(1,4,3)
+plot(abs(r3))
+subplot(1,4,4)
+plot(abs(r4))
 
-signal=100*rand(1000,1);
