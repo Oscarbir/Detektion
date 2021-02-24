19cab6d30d7ff0313c55fc40db58c84450655cac
Marcus <lindohf@student.chalmers.se>
2021-02-24 14:57:40 +0100

    generate signal with bw -> c_signalgen

--- Detektion.m
+++ Detektion.m
@@ -3,6 +3,7 @@ clc, clear all
 %%T�nker att vi skapar en signal f�rst 
 X=load('x.mat');
 x=X.x;
+
 r1=xcorr(x(:,1),x(:,2),3000)/length(x(:,1));
 r2=xcorr(x(:,1),x(:,3),3000)/length(x(:,1));
 r3=xcorr(x(:,1),x(:,4),3000)/length(x(:,1));
@@ -16,3 +17,22 @@ plot(abs(r3))
 subplot(1,4,4)
 plot(abs(r4))
 
+figure(2)
+
+pspectrum(x(:,1))
+
+%% genSignal
+close,clear all,clc
+
+sParam=struct;
+sParam.centerFq=0;
+sParam.bw=3e6;
+sParam.gain=30;
+sParam.fs=10e6;
+
+
+signalGen=c_SignalGen(sParam);
+[fAxis,s]=signalGen.generateSignal;
+
+y=10
+plot(fAxis,abs(s))
--- c_SignalGen.m
+++ c_SignalGen.m
@@ -4,18 +4,35 @@ classdef c_SignalGen
     properties
         centerFq;
         bw;
-        samples;
+        gain;
+        fs;
+        N;
+        
     end
     
     methods
-        function obj = c_SignalGen(centerFq,bw)
-            obj.centerFq=centerFq;
-            obj.bw=bw;
-            
+        function obj = c_SignalGen(sParam)
+            obj.centerFq=sParam.centerFq;
+            obj.bw=sParam.bw;
+            obj.gain=sParam.gain;
+            obj.fs=sParam.fs;
+            obj.N=obj.fs;
         end
         
-        function out = generateSignal(obj)
+        function [fAxis,out] = generateSignal(obj)
             
+            df=obj.N/obj.fs; % delta f of sample
+            k=obj.bw/df; % number of smaples for bw
+            
+            noiseFloor=randn(obj.N,1)+1i*rand(obj.N,1); %noisefloor
+            noiseSignal=randn(k,1)+ 1i*randn(k,1);
+            
+            %zeropad to match length of noisefloor, gain added to signal
+            zPaddedNoiseSignal=[zeros(obj.N/2-k/2,1);noiseSignal+obj.gain;zeros(obj.N/2-k/2,1,1)];
+            
+            out=zPaddedNoiseSignal+noiseFloor;
+            
+            fAxis = (-obj.N/2:obj.N/2-1)*(obj.fs/obj.N);
         end
     end
 end
