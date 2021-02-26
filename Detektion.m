32a32b463cc75feb243cbd15c8dc9b60b068e580
Oscar Birging <oscarbi@student.chalmers.se>
2021-02-26 09:18:40 +0100

    Inget speciellt

--- Detektion.m
+++ Detektion.m
@@ -1,18 +1,25 @@
 clc, clear all
-
-%%T�nker att vi skapar en signal f�rst 
 X=load('x.mat');
 x=X.x;
-r1=xcorr(x(:,1),x(:,2),3000)/length(x(:,1));
-r2=xcorr(x(:,1),x(:,3),3000)/length(x(:,1));
-r3=xcorr(x(:,1),x(:,4),3000)/length(x(:,1));
-r4=xcorr(x(:,1),x(:,5),3000)/length(x(:,1));
- subplot(1,4,1)
+
+%%
+% x1=x(:,1);
+% x2=x(:,2);
+r1=xcorr(x(:,1),x(:,2),10000)/length(x(:,1));
+
+ %subplot(1,4,1)
 plot(abs(r1))
- subplot(1,4,2)
- plot(abs(r2))
-subplot(1,4,3)
-plot(abs(r3))
-subplot(1,4,4)
-plot(abs(r4))
+
+% [afmag,delay,doppler] = ambgfun(x1(1:200),x2(1:200),10e6,[100000 100000]);
+% surf(delay*1e6,doppler/1e3,afmag,'LineStyle','none');
+% axis tight; grid on; view([140,35]); colorbar;
+% xlabel('Delay \tau (us)');ylabel('Doppler f_d (kHz)');
+% title('Linear FM Pulse Waveform Ambiguity Function');
+
+%%
+
+y=fft(x(:,1));
+t=linspace(1,1/10e6,10000);
+plot(t,abs(y))
+
 
